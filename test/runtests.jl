59ff4f659c79bac7e5c5fb3936fe3e242549f090
Vojtech Kumpost <vojtech.kumpost@kit.edu>
2021-12-07 13:06:49 +0100

    Add find_all_combinations

--- test/runtests.jl
+++ test/runtests.jl
@@ -68,4 +68,17 @@ using MiscFunctions
     yy = smooth(y, 100)
     @test all(yy .≈ [-2, -2, -0.8, 0.2857142857142857, 1.8, 3, 6])
 
+    # find_all_combinations
+    vectors = [[1, 0, 2]]
+    combinations = find_all_combinations(vectors)
+    @test combinations == hcat([1, 0, 2])
+
+    vectors = [[1, 0, 2], [7, 6]]
+    combinations = find_all_combinations(vectors)
+    @test combinations == [1 7; 1 6; 0 7; 0 6; 2 7; 2 6]
+
+    vectors = [[1, 0, 2], ['a'], [7, 6]]
+    combinations = find_all_combinations(vectors)
+    @test combinations == [1 'a' 7; 1 'a' 6; 0 'a' 7; 0 'a' 6; 2 'a' 7; 2 'a' 6]
+
 end
